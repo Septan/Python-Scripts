42897fa19e0dfebf05904e2fba9cad7c005fca9c
Septan <sinexpond1987@gmail.com>
2023-05-11 03:13:49 -0500

    Create file_organizer.py

---
+++ File Organizer/file_organizer.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+# Define the folder structure you want to create
+folders = {
+    "Documents": [".pdf", ".doc", ".docx", ".txt", ".rtf"],
+    "Photos": [".jpg", ".jpeg", ".png", ".gif"],
+    "Music": [".mp3", ".wav"],
+    "Videos": [".mp4", ".avi", ".flv", ".mkv"],
+    "Compressed": [".zip"],
+    "Python Files": [".py"],
+    "Batch Files": [".bat"]
+}
+
+# Get the paths for the source and destination folders from user input
+src_folder = input("Enter the path of the source folder: ")
+dest_folder = input("Enter the path of the destination folder: ")
+
+# Create the folder structure in the destination folder
+for folder in folders:
+    folder_path = os.path.join(dest_folder, folder)
+    os.makedirs(folder_path, exist_ok=True)
+
+# Move files to the appropriate folders (including subfolders)
+for root, dirs, files in os.walk(src_folder):
+    for file_name in files:
+        src_path = os.path.join(root, file_name)
+        if os.path.isfile(src_path):
+            for folder_name, extensions in folders.items():
+                for extension in extensions:
+                    if file_name.endswith(extension):
+                        dest_path = os.path.join(dest_folder, folder_name, file_name)
+                        shutil.move(src_path, dest_path)
+                        break
+
+# Delete empty folders from source folder
+for root, dirs, files in os.walk(src_folder, topdown=False):
+    for dir_name in dirs:
+        dir_path = os.path.join(root, dir_name)
+        if not os.listdir(dir_path):
+            os.rmdir(dir_path)
+
+print("File organization complete!")
