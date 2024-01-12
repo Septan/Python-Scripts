dbcce0b40388d37fbdb7e9a97e339499e826e589
Septan <sinexpond1987@gmail.com>
2024-01-12 01:52:34 -0600

    Update master.py

--- master.py
+++ master.py
@@ -3,17 +3,12 @@ import subprocess
 
 # Define the available scripts with descriptions
 scripts = [
-<<<<<<< HEAD
     ("File Organizer/file_organizer.py", "Organizes files in a directory."),
     ("File Organizer/unused_file_mover.py", "Moves unused files to a specified location."),
     ("MKV to MP4 Converter 1/meta.py", "Processes a batch of mkv files convert to mp4 while adding metadata information."),
     ("MKV to MP4 Converter 2/meta_subtitle.py", "Same as 3 but adds subtitles."),
-    ("Video Highlight Extractor/video_hightlight_extractor.py", "It extracts highlight clips based on detected scenes, saving them with audio in a specified output folder."),
+    ("Video Highlight Extractor/video_highlight_extractor.py", "It extracts highlight clips based on detected scenes, saving them with audio in a specified output folder."),
     ("Video Frame Extraction Tool/frame_extractor.py", "Extract frames from video file based on SSIM and PSNR thresholds."),
-=======
-    "File Organizer/file_organizer.py",
-    "File Organizer/unused_file_mover.py",
->>>>>>> parent of 5b6e871 (Update master.py)
 ]
 
 # Display available scripts with descriptions
@@ -31,7 +26,7 @@ if selected_index < 1 or selected_index > len(scripts):
 
 # Get the path to the selected script
 script_name, _ = scripts[selected_index - 1]
-script_path = os.path.normpath(os.path.join(os.getcwd(), "Scripts", script_name))
+script_path = os.path.normpath(os.path.join(os.getcwd(), script_name))
 
 # Determine the execution method based on the file extension
 file_extension = os.path.splitext(script_path)[1].lower()
